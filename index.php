d7df3f6bafae84ec8d42412c2ff46fd22665312f
jestes0 <39225989+jestes0@users.noreply.github.com>
2020-06-03 10:19:46 -0400

    Add files via upload
    Basic files to add to the website. All files are a WIP.

---
+++ index.php
@@ -0,0 +1,52 @@
+<html>
+<title>
+  Codemans Hub
+</title>
+<head color = #BBBBBB><h1>Codemans Development and Blog</h1></head>
+<body>
+<ul class = "navbar">
+<li class = "navelement"><a class = "navlink" href = index.php>Home</a></li>
+<li class = "navelement"><a class = "navlink" href = CSCI297/index.html>Dev Blog</a></li>
+<li class = "navelement"><a class = "navlink" href = CSCI432/>Blog</a></li>
+<li class = "navelement"><a class = "navlink" href = CSCI440/index.html>About Me</a></li>
+<li class = "navelement"><a class = "navlink" href = contact.html>Contact</a></li>
+</ul>
+
+<style>
+li{
+float: left;
+border-right: 1px solid #ffffff;
+}
+
+li a{
+display: inline;
+color: white;
+text-align: center;
+padding: 18px 50px;
+text-decoration: none;
+}
+
+li:last-child{
+border-right:none;
+}
+
+li a:hover{
+background-color: #111111;
+}
+
+ul {
+overflow: hidden;
+background-color: #333333;
+list-style-type: none;
+margin: 0;
+padding: 15;
+}
+</style>
+
+<p> Thank you for visiting my website. I hope to graduate in May.
+<br>
+<p>Please use the Navigation Bar above to select my featured work from my classes.
+
+
+</body>
+</html>
